df5c0f2526c9d37c9123dafc61ba607ff7262040
Mankurt543 <55107569+Mankurt543@users.noreply.github.com>
2019-12-26 04:15:17 +0300

    ajsdkjn

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 let mongoose=require("mongoose")
 mongoose.Promise=require("bluebird")
-var mongodb="mongodb://localhost/mehmet"
+var mongodb="mongodb+srv://memet:Mankurt543aa@sondeneme-2ieho.mongodb.net/test?retryWrites=true&w=majority"
 mongoose.set('useCreateIndex', true)
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
@@ -8,9 +8,7 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
 module.exports=mongoose.connect(mongodb,function(err,db){
-    if(err){
-        console.log("veritabanına bağlanamadı")
-    }
+    if(err) throw err
     else{
         console.log(mongodb +"bağlandı")
       
